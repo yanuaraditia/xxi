a519b9e70c453109e26061ea5218bf78e1f426f0
yanuar <yanuaraditia@outlook.com>
2020-02-16 07:55:10 +0700

    VUE

--- main.js
+++ main.js
@@ -1,26 +1,55 @@
-function scrollFunction() {
-    var hei = $('.header').height();
-    if (document.body.scrollTop > hei || document.documentElement.scrollTop > hei) {
-        $('.header').addClass('is-sticky');
+// 1. Definisikan komponen Vue
+// Komponen "Home" akan menampilkan halaman depan aplikasi
+const Home = Vue.extend({ 
+    template: '#home',
+    data: function() {
+        return {
+            judul:'!'
+        }        
     }
-    else {
-        $('.header').removeClass('is-sticky');
-    }
-};
-$(document).ready(function() {
-    $(".navbar-burger").click(function() {
-        $(".navbar-burger").toggleClass("is-active");
-        $(".navbar-menu").toggleClass("is-active");
-    });
-    scrollFunction();
 });
-window.onscroll = function() {
-    scrollFunction();
-};
-window.Prism = window.Prism || {};
-window.Prism.manual = true;
-$('.js-tilt').tilt({
-    scale: 1.05,
-    glare: true,
-    maxGlare: .2
-})
+const Work = Vue.extend({ 
+    template: '#work',
+    data: function() {
+        return {
+            kerjaan: [
+                {
+                    id: 1,
+                    url: 'https://imaka.or.id',
+                    title: 'IMAKA',
+                    img: 'images/work1.png',
+                    type: 'CMS'
+                },
+                {
+                    id: 2,
+                    url: 'https://paparkir.com',
+                    title: 'Paparkir',
+                    img: 'images/work2.png',
+                    type: 'Landing'
+                },
+                {
+                    id: 3,
+                    url: 'https://baleritaflorist.com',
+                    title: 'Balerita Florist',
+                    img: 'images/work3.png',
+                    type: 'CMS'
+                }
+            ]
+        }
+    }
+});
+const About = Vue.extend({ 
+    template: '#about',
+    data: function() {
+        return {
+            judul:'!'
+        }        
+    }
+});
+const routes = [
+    { path: '/', component: Home},
+    { path: '/about', component: About },
+    { path: '/work', component: Work }
+];
+const router = new VueRouter({ routes:routes })
+const spa = new Vue({ router }).$mount('#main-wrapper');
